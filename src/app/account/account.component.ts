30e430003a5117b351d82ba3f6e3adc14656281e
ribu.singh <ribu.singh@gritsa.com>
2021-03-24 16:42:01 +0530

    ui fixes

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -15,8 +15,8 @@ export class AccountComponent implements OnInit {
     { time: '14-Feb 5:15 PM', coinCredited: '54' },
     { time: '18-Feb 5:45 AM', coinCredited: '40' },
     { time: '19-Feb 4:15 PM', coinCredited: '55' },
-    { time: '20-Mar 8:15 PM', coinCredited: '28' },
-    { time: '25-Mar 8:15 AM', coinCredited: '5' },]
+    { time: '20-Feb 8:15 PM', coinCredited: '28' },
+    { time: '25-Feb 8:15 AM', coinCredited: '5' },]
   }
 
   ngOnInit() { }
